a9f4d77f97d60da0d03ea2d43d37f8518e1019cf
olivia-johnson <64459311+olivia-johnson@users.noreply.github.com>
2020-06-01 11:32:03 +0930

    Summary statistic for slim output

---
+++ slim_sumstats.R
@@ -0,0 +1,36 @@
+library(popgen.tools)
+setwd("~/work/PhD/Drosophila/")
+
+## read in SLiM output (MS)
+raw <- scan(
+  file = "~/work/PhD/Drosophila/slim_fs_output.txt",
+  what = character(0),
+  sep = "\n",
+  quiet = TRUE
+)
+
+## Sep genotypes 
+genotypeM = t(sapply(raw[4:length(raw)], function(x) as.numeric(strsplit(x, split = "")[[1]]), USE.NAMES = F))
+
+#extract positions of seg sites
+pos = unlist(strsplit(raw[3], split=" "))
+p = list(pos[2:length(pos)])
+p
+
+
+# calculate sum_stats using popgen.tools
+obs <- sim_obj(cmd = NA,
+               seeds = NA,
+               segsites = ncol(genotypeM),
+               positions = p,
+               genome_matrix = genotypeM,
+               sweep ="partial",
+               select_coeff = 0.3 )
+
+obs_ss = sum_stats(
+  sim = obs,
+  nwins = 2,
+  ID = 1, 
+  split_type = "mut",
+  snp=100, #trim genome matrix
+)
