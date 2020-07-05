fe8d368c2789447a9654497201a95b1ae37634ff
olivia-johnson <64459311+olivia-johnson@users.noreply.github.com>
2020-07-05 21:28:26 +0930

    Reorder, add summary statistics

--- simulations.py
+++ simulations.py
@@ -1,6 +1,13 @@
 import os
+os.chdir("/Users/olivia/oliviaphd/")
+
 import msprime
 import pyslim
+import tskit
+import numpy as np
+import pandas as pd
+
+
 
 genomeSize = 1000
 popnSize = 1000
@@ -10,28 +17,40 @@ l = 100
 y = 2.0
 d = 0.6
 
+## COALESCENT BURN IN
 
+#burnin = msprime.simulate(sample_size=2*popnSize,Ne=popnSize, length=genomeSize, mutation_rate=mutRate, recombination_rate=recRate)
+#burnin_ts = pyslim.annotate_defaults(simp, model_type="WF", slim_generation=1. reference_sequence=None)
+#burnin_ts.dump("./burnin.trees")
 
-## Call simulation
+## FORWARD SIMULATION
 
-cmd = "slim -d GenomeSize=" + str(genomeSize) + " -d L=" + str(l)+ " -d N=" + str(popnSize) + " -d y=" + str(y) + " -d d=" + str(d) + " -d mut=0.0 -d rr=" + str(recRate) +" seglift.slim"
+cmd = "slim -d GenomeSize=" + str(genomeSize) + " -d L=" + str(l)+ " -d N=" + str(popnSize) + " -d y=" + str(y) + " -d d=" + str(d) + " -d mut=0.0 -d rr=" + str(recRate) +" ~/oliviaphd/seglift.slim"
 print(cmd)
 os.system(cmd)
 
-sim_ts = pyslim.load("./treeseq.trees")
+slim_ts = pyslim.load("./treeseq.trees").simplify()
 
-## burn in
+slim_ts.num_mutations
 
-#ts = msprime.simulate(sample_size = 100, Ne=popnSize, length=genomeSize, mutation_rate=mutRate, recombination_rate=recRate)
-#slim_ts = pyslim.annotate_defaults(ts, model_type="WF", slim_generation=1)
-#slim_ts.dump("./burnin.trees")
+## ADD NEUTRAL MUTATIONS
+mut_ts = pyslim.SlimTreeSequence(msprime.mutate(slim_ts, rate=mutRate, keep=True))
 
-
-burnin_ts = pyslim.SlimTreeSequence(msprime.mutate(sim_ts, rate=1e-6, keep=True))
-
-
-## Format Output
+for mut in mut_ts.mutations():
+    md = pyslim.decode_mutation(mut.metadata) 
+  
 
 
 
-## Summary Statistics
+## SUMMARY STATISTICS
+
+##Tajima's D - make sample_sets list of lists when sampling over time
+win = np.linspace(0, genomeSize, num=11)
+win = win.astype(int)
+tajd = mut_ts.Tajimas_D(sample_sets=None, windows=win, mode="site")
+
+sum_stats = pd.DataFrame([win[0:10], (win[1:11]-1), tajd], ["win_start", "win_end", "Tajimas D"])
+sum_stats.pivot(columns='var', values='val')
+
+
+sum_stats = pd.DataFrame({"win_start" : win[0:10], "win_end" :(win[1:11]-1), "Tajimas D": tajd}, index = pd.MultiIndex.from_tuples([(2000,1), (2000,2), (2000,3),(2000,4),(2000,5),(2000,6),(2000,6),(2000,7),(2000,8),(2000,9),], names=["Generation", "Window"]))
