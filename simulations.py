b136642a08e263b56294ca5e91d6e38f87f1c66d
olivia-johnson <64459311+olivia-johnson@users.noreply.github.com>
2020-07-20 20:40:14 +0930

    Add allele freq, edit summary statistics.

--- simulations.py
+++ simulations.py
@@ -9,13 +9,17 @@ import pandas as pd
 
 
 
-genomeSize = 1000
-popnSize = 1000
+genomeSize = 1e4
+popnSize = 100
 mutRate = 1e-6
 recRate = 1e-8
 l = 100
 y = 2.0
 d = 0.6
+nWin = 10
+rGen = 100
+#g_s = no. summer generations
+#g_w = no. winter generations
 
 ## COALESCENT BURN IN
 
@@ -27,32 +31,80 @@ burnin_ts.dump("burnin.trees")
 
 ## FORWARD SIMULATION
 
-cmd = "slim -d GenomeSize=" + str(genomeSize) + " -d L=" + str(l)+ " -d N=" + str(popnSize) + " -d y=" + str(y) + " -d d=" + str(d) + " -d mut=0.0 -d rr=" + str(recRate) +" ~/oliviaphd/seglift.slim"
+cmd = "slim -d GenomeSize=" + str(int(genomeSize)) + " -d L=" + str(l)+ " -d N=" + str(int(popnSize)) + " -d y=" + str(y) + " -d d=" + str(d) + " -d mut=0.0 -d rr=" + str(recRate) +" ~/oliviaphd/seglift.slim"
 print(cmd)
 os.system(cmd)
 
 slim_ts = pyslim.load("./treeseq.trees").simplify()
 
-slim_ts.num_mutations
+## mutation check
+if (slim_ts.num_sites != l):
+    print("Less than " + str(l) + "introduced mutations")
+else:
+    print (str(l) + " introduced mutations")
+## SUMMARISE MUTATIONS
+mut_met = pd.DataFrame({"mut_num": [], "mut_pos": [], "mut_type":[]})
+for mut in slim_ts.mutations():
+    #print(mut)
+    mut_met = mut_met.append({"mut_num" : [mut.site], "mut_pos" : [(mut.position)], "mut_type":["fluctuating"]}, ignore_index=True)
 
+mut_met = mut_met.loc[mut_met.astype(str).drop_duplicates(subset=("mut_num")).index]
+    
+
+def allele_counts(ts, sample_sets=None):
+    if sample_sets is None:
+       sample_sets = [ts.samples()]
+    def f(x):
+       return x
+    return ts.sample_count_stat(sample_sets, f, len(sample_sets),
+               span_normalise=False, windows='sites',
+               polarised=True, mode='site', strict=False)
+
+alco = allele_counts(slim_ts, sample_sets = None) ## ASSUMING IN RIGHT ORDER
+mut_met["allele_count"] = alco
+mut_met.assign(allele_freq=lambda df: mut_met.allele_count/(2*popnSize))
+
+
+## SUMMARISE INDIVIDUALS
+ind_met = pd.DataFrame({"id": [], "time": [], "pop": [], "season" : []})   
+for ind in slim_ts.individuals():
+  #  print(ind)
+    if ind.time % 10 == 0:
+        ind_season = "S"
+    else:
+        ind_season = "W"
+    ind_met = ind_met.append({"id" : [ind.id], "time" : [ind.time], "pop" : [ind.population], "season" : [ind_season]}, ignore_index=True)
+
+    
 ## ADD NEUTRAL MUTATIONS
 mut_ts = pyslim.SlimTreeSequence(msprime.mutate(slim_ts, rate=mutRate, keep=True))
 
-for mut in mut_ts.mutations():
-    md = pyslim.decode_mutation(mut.metadata) 
+
   
-
-
-
 ## SUMMARY STATISTICS
-
 ##Tajima's D - make sample_sets list of lists when sampling over time
-win = np.linspace(0, genomeSize, num=11)
-win = win.astype(int)
-tajd = mut_ts.Tajimas_D(sample_sets=None, windows=win, mode="site")
-
-sum_stats = pd.DataFrame([win[0:10], (win[1:11]-1), tajd], ["win_start", "win_end", "Tajimas D"])
-sum_stats.pivot(columns='var', values='val')
+#win = np.linspace(0, genomeSize, num=wins+1)
+#win = win.astype(int)
 
 
-sum_stats = pd.DataFrame({"win_start" : win[0:10], "win_end" :(win[1:11]-1), "Tajimas D": tajd}, index = pd.MultiIndex.from_tuples([(2000,1), (2000,2), (2000,3),(2000,4),(2000,5),(2000,6),(2000,6),(2000,7),(2000,8),(2000,9),], names=["Generation", "Window"]))
+sum_stats = pd.DataFrame({"generation": [],  "win_start": [], "win_end" :[], "tajimas_d": [], "afs":[]})
+#"allele_freq":[],
+#"win_num":[],
+
+
+def sum_stats(ts, wins):
+    #create windows
+    win = np.linspace(0, ts.sequence_length, num=wins+1)
+    
+    # calculate Tajima's D for windows
+    tajd =  ts.Tajimas_D(sample_sets=None, windows=win, mode="site")
+    
+    #calculate allele frequency spectrum
+    fs = ts.allele_frequency_spectrum(sample_sets=None, windows = win, span_normalise=False, polarised=True)
+    
+    
+    #Fill in df
+    #sum_stats = sum_stats.append({"generation": [gen],  "win_start" : win[0:wins], "win_end":[(win[1:(wins+1)]-1)], "tajimas_d": [tajd], "afs":[fs]}, ignore_index=True))
+    
+    df = pd.DataFrame({"win_start" : win[0:wins], "win_end":[(win[1:(wins+1)]-1)], "tajimas_d": [tajd], "afs":[fs]}, index = pd.MultiIndex.from_tuples([(gen, (1:wins))], names=['generation','n_win']))
+    
