2055c6bab8982a030427b0121351a9bfa78b893e
olivia-johnson <64459311+olivia-johnson@users.noreply.github.com>
2020-07-22 21:09:16 +0930

    Add fitness calculation

--- simulations.py
+++ simulations.py
@@ -18,8 +18,8 @@ y = 2.0
 d = 0.6
 nWin = 10
 rGen = 100
-#g_s = no. summer generations
-#g_w = no. winter generations
+sum_gen = 8#no. summer generations
+win_gen = 3#no. winter generations
 
 ## COALESCENT BURN IN
 
@@ -30,7 +30,7 @@ burnin_ts = pyslim.annotate_defaults(burnin, model_type="WF", slim_generation=1)
 burnin_ts.dump("burnin.trees")
 
 ## FORWARD SIMULATION
-
+# for when uneven seasons " -d g_s=" + str(sum_gen)+ " -d g_w=" + str(win_gen)
 cmd = "slim -d GenomeSize=" + str(int(genomeSize)) + " -d L=" + str(l)+ " -d N=" + str(int(popnSize)) + " -d y=" + str(y) + " -d d=" + str(d) + " -d mut=0.0 -d rr=" + str(recRate) +" ~/oliviaphd/seglift.slim"
 print(cmd)
 os.system(cmd)
@@ -46,7 +46,7 @@ else:
 mut_met = pd.DataFrame({"mut_num": [], "mut_pos": [], "mut_type":[]})
 for mut in slim_ts.mutations():
     #print(mut)
-    mut_met = mut_met.append({"mut_num" : [mut.site], "mut_pos" : [(mut.position)], "mut_type":["fluctuating"]}, ignore_index=True)
+    mut_met = mut_met.append({"mut_num" : mut.site, "mut_pos" : mut.position, "mut_type":"fluctuating"}, ignore_index=True)
 
 mut_met = mut_met.loc[mut_met.astype(str).drop_duplicates(subset=("mut_num")).index]
     
@@ -66,14 +66,37 @@ mut_met.assign(allele_freq=lambda df: mut_met.allele_count/(2*popnSize))
 
 
 ## SUMMARISE INDIVIDUALS
-ind_met = pd.DataFrame({"id": [], "time": [], "pop": [], "season" : []})   
+ind_met = pd.DataFrame({"id": [], "time": [], "pop": [], "season" : [], "nodes":[], "s_fitness":[], "w_fitness":[]})   
 for ind in slim_ts.individuals():
+ 
   #  print(ind)
     if ind.time % 10 == 0:
         ind_season = "S"
     else:
         ind_season = "W"
-    ind_met = ind_met.append({"id" : [ind.id], "time" : [ind.time], "pop" : [ind.population], "season" : [ind_season]}, ignore_index=True)
+        
+    ind_met = ind_met.append({"id" : ind.id, "time" : ind.time, "pop" : ind.population, "season" : ind_season, "nodes": ind.nodes}, ignore_index=True)
+
+## CALCULATE FITNESS
+samps = ind_met[["id", "nodes"]]
+counts = allele_counts(slim_ts, sample_sets = list(samps.nodes))
+
+for i in len(counts[1]):
+    s_count = counts[:,i]
+    ns = sum(s_count == 2)
+    n_het = sum(s_count == 1)
+    nw = sum(s_count == 0)
+    z_s = ns + (d*nhet)
+    z_w = nw + (d*nhet)
+    s_fit = (1+z_s)^y
+    w_fit = (1+z_w)^y
+    if samps.nodes[i] == ind_met.nodes[i]:
+        ind_met.loc[ind_met.id == samps.id[i], ["s_fitness", "w_fitness"]] = [s_fit, w_fit]
+    else:
+        print("Nodes do not match for sample " + str(i))
+    
+
+#for i in list(np.unique(ind_met.time)):
 
     
 ## ADD NEUTRAL MUTATIONS
@@ -102,7 +125,7 @@ def sum_stats(ts, wins):
     #calculate allele frequency spectrum
     fs = ts.allele_frequency_spectrum(sample_sets=None, windows = win, span_normalise=False, polarised=True)
     
-    
+    ## SAMPLE BY WINDOW???  allele_counts(ts,sample_sets=None)
     #Fill in df
     #sum_stats = sum_stats.append({"generation": [gen],  "win_start" : win[0:wins], "win_end":[(win[1:(wins+1)]-1)], "tajimas_d": [tajd], "afs":[fs]}, ignore_index=True))
     
