da66500c024239d73cafb2ae6504efb5d142bc2a
olivia-johnson <64459311+olivia-johnson@users.noreply.github.com>
2021-03-22 12:40:24 +1030

    Fix and test positions for allel stats, and windows for allel stats

--- ts_analysis.py
+++ ts_analysis.py
@@ -16,14 +16,14 @@ import itertools
 import allel
 
 #groups = (4, 5)
-group =4 ## identifier for a set of parameters
+group =7 ## identifier for a set of parameters
 runs = 2
 #genomeSize = int(1e6)
-nChrom = 5
+nChrom = 10
 popnSize = int(1e4)
 mutRate = 1e-6
 recRate = 1e-8
-l = 5
+l = 10
 nWin = 101 * nChrom
 sum_gen = 15#no. summer generations
 win_gen = 15#no. winter generations
@@ -59,7 +59,8 @@ sim_run = 1
 #         print(sim_run)
         
 def ts_analysis(sim_run):
-        slim_ts = pyslim.load("./group_{0}/treeseq_{1}_{2}_{3}.trees".format(group,sim_type,group,sim_run)).simplify()
+       ## burnin =  pyslim.load("./group_{0}/burnin_seglift_long_{0}_{1}.trees".format(group,sim_run))
+        slim_ts = pyslim.load("./group_{0}/treeseq_{1}_{0}_{2}.trees".format(group,sim_type,sim_run)).simplify()
         genomeSize = slim_ts.sequence_length
         ## Check number of mutations that were introduced
         if (slim_ts.num_sites != l):
@@ -67,26 +68,23 @@ def ts_analysis(sim_run):
         else:
             print (str(l) + " introduced mutations")
             
+        #### check intervals
+            
         ## SUMMARISE MUTATIONS
         
         #start_time = time.time()
             ## obtain data for selected sites (used later to remove from tree sequence)
-        mut_met = pd.DataFrame({"mut_num": [], "mut_pos": [], "site_id":[]})
+        mut_met = pd.DataFrame({"mut_site": [], "mut_pos": [], "mut_id":[]})
         for mut in slim_ts.mutations():## run through muts in ts
             #print(mut)
-            mut_met = mut_met.append({"mut_num" : mut.site, "mut_pos" : mut.position, "site_id" : mut.id}, ignore_index=True)
+            mut_met = mut_met.append({"mut_site" : mut.site, "mut_pos" : mut.position, "mut_id" : mut.id}, ignore_index=True)
             ## remove replicated mutations (as mutations added in SLiM by randomly sampling individuals, goes against tree structure of burnin hence sometimes have duplicates)
        # t = slim_ts.dump_tables()
        # t.deduplicate_sites()   ### cannot get deduplicate to work
        
-        #ts = slim_ts.delete_sites(list(mut_met.site_id.astype(int)))
-       
-           
+       # print("Time for initial mut info = ", (time.time()- start_time))
         
-       #mut_met = mut_met.loc[mut_met.astype(str).drop_duplicates(subset=("mut_num")).index] ## doesnt remove duplicates from tree only table
-           # print("Time for initial mut info = ", (time.time()- start_time))
-        
-        
+
         ## SUMMARISE INDIVIDUALS
         #start_time = time.time()
         rows_list = []
@@ -113,11 +111,11 @@ def ts_analysis(sim_run):
         ind_met = pd.DataFrame(rows_list) ## convert from dictionary to data frame (fast)
         #print("Time for ind table = ", (time.time()- start_time))
         
-        
-            
+        ## REMOVE SEELCTED MUTATIONS
+        no_mut_ts = slim_ts.delete_sites(list(mut_met.mut_site.astype(int)))    
         ## ADD NEUTRAL MUTATIONS
         #start_time = time.time()
-        mut_ts = pyslim.SlimTreeSequence(msprime.mutate(slim_ts, rate=mutRate, keep=True))
+        mut_ts = pyslim.SlimTreeSequence(msprime.mutate(no_mut_ts, rate=mutRate, keep=True))
         ##discrete = True
         #print("Time to ad neutral muts = ", (time.time()- start_time))
         
@@ -130,48 +128,42 @@ def ts_analysis(sim_run):
             ## obtain data for neutral mutations added to ts
         for mut in mut_ts.mutations(): ## run through muts in ts
             #print(mut)
-        
-                #print(mut)
-                ## calculate distance from nearest seleted mutation (commented to increase speed)
-            # given_value = mut.position
-            # absolute_difference_function = lambda list_value : abs(list_value - given_value)
-            # closest_value = min(a_list, key=absolute_difference_function)
-            # dist = given_value - closest_value
-            #num = mut_met.mut_num[mut_met.mut_pos == closest_value]
-            
-                ## differentiate mutation type
-            if mut.metadata["mutation_list"] == []:
-                m_type = "neutral"
-            else:
-                m_type = "fluctuating"
-                
+  
             dict2 = {}
                 # get input row in dictionary format
                 # key = col_name
             dict2.update({"mut_id":mut.id})                     ## id of mutation in ts
-            dict2.update({"mut_num" : mut.site})                ## number of mutation
+            dict2.update({"mut_site" : mut.site})                ## number of mutation
             dict2.update({"mut_pos" : mut.position})            ## position of mutation
-            dict2.update({"mut_type":m_type})                   ## fluctating or neutral
-            # dict2.update({"nearest_dist": abs(dist)})         ## distance between neutral mut and nearest selected loci
-            # dict2.update({"nearest_mut_pos": closest_value})  ## position of nearest selected loci
-           # dict2.update({"nearest_mut_num": int(num)})        ## number of nearest seelcted loci
         
             rows_list2.append(dict2)
         n_met = pd.DataFrame(rows_list2) ## convert dict to df
         
+            ## emsure all mut positions are unique
+        site_arr = np.rint(5e6*n_met['mut_pos'].values)
+
+        tot_size = np.size(site_arr)
+
+        unq_size = np.size(np.unique(site_arr))
+        
+        print((tot_size - unq_size), " identical positions.")
+
+        for i in range(len(site_arr)-1):
+            if site_arr[i]>=site_arr[i+1]:
+                print("Site: ",i)
+                site_arr[i+1] = site_arr[i] + 1
+                
+        ##add unqiue positions to n_met df
+        n_met['pos_corr'] = site_arr
+
+        cor_tot_size = np.size(n_met.pos_corr)
+
+        cor_unq_size = np.size(np.unique(n_met.pos_corr))
+        
+        print((cor_tot_size - cor_unq_size), " identical positions after correction.")
         
         #print("Time for mut table = ", (time.time()- start_time))
         
-        
-        
-        ## REMOVE SELECTED SITES FROM TS
-            ##format fluctating loci into list
-        list_m = n_met.mut_num[n_met.mut_type=="fluctuating"].astype(int)
-        list_mu = list_m .tolist()
-        #mut_met = mut_met.loc[mut_met.astype(str).drop_duplicates(subset=("mut_num")).index]
-            ## remove selected loci from ts for analysis of neutral loci
-        #stat_ts = pyslim.SlimTreeSequence(mut_ts.delete_sites(list_mu, record_provenance=True))
-        stat_ts = mut_ts.delete_sites(list_mu, record_provenance=True)
           
         ## CALCULATE SUMMARY STATISTICS
         
@@ -179,7 +171,15 @@ def ts_analysis(sim_run):
         
         #start_time = time.time()
             ## create windows for stats to be calculated in
-        win = np.linspace(0, stat_ts.sequence_length, num=nWin+1).astype(int)
+        win = np.linspace(0, mut_ts.sequence_length, num=nWin+1).astype(int)
+        
+        al_win = []
+        for w in range(len(win)-1):
+            if w == nWin:
+                window = [win[w], int(genomeSize)]
+            else:
+                window = [win[w], win[w+1]-1]
+            al_win.append(window)
         #print("create windows = ", (time.time()- start_time))
         start_time = time.time()
             ## seperate into ts for each timepoint
@@ -192,7 +192,7 @@ def ts_analysis(sim_run):
                 ## create ts of just samples at time t using list
                 ## used by tskit to calculate states based on tree structure
             #samp_ts = stat_ts.subset(samples)              ## may not be keeping history of nodes
-            samp_ts = stat_ts.simplify(samples = samples)   ## should keep history of nodes
+            samp_ts = mut_ts.simplify(samples = samples)   ## should keep history of nodes
             #print("get samples = ", (time.time()- s_t))
                 ## create genotype array to put into scikit-allel  shape ==(var, ind)
             samp_gm=samp_ts.genotype_matrix()  
@@ -205,10 +205,10 @@ def ts_analysis(sim_run):
             #samp_hm = np.asarray(samp_gm)
             
             
-            ##Check all sites of gm are polymorphic
-            for i in range(len(samp_gm)):
-                if sum(samp_gm[i]) == 0:
-                    print (str(i) + "is not polymoprhic")
+            # ##Check all sites of gm are polymorphic
+            # for i in range(len(samp_gm)):
+            #     if sum(samp_gm[i]) == 0:
+            #         print (str(i) + "is not polymoprhic")
                     
                     
             ## convert to haplotyoe array
@@ -217,7 +217,7 @@ def ts_analysis(sim_run):
          
             #h_stat = allel.moving_garud_h(h, size = int(genomeSize/nWin), step=None)
             
-            size_factor = 1e15 # HARD coded
+            size_factor = 1 # HARD coded was 1e15
             positions = []
             for mut in samp_ts.mutations():
                 positions.append(int(mut.position*size_factor))
@@ -230,7 +230,7 @@ def ts_analysis(sim_run):
             #if len(np.unique(mut_positions.astype(int))) != len(mut_positions):
                # print("Multiple mutattions at single pos")
             
-            hap_stats = allel.windowed_statistic(mut_positions,h,allel.garud_h,size=int((genomeSize/nWin)*size_factor), start = 0, stop = int((genomeSize-1)*size_factor))
+            hap_stats = allel.windowed_statistic(mut_positions,h,allel.garud_h, windows = al_win)
             
         
         ## calculate Tajima's D 
@@ -239,13 +239,20 @@ def ts_analysis(sim_run):
                 ## using allel
             # taja = allel.windowed_tajima_d(genotypes, windows = win)
             
-            theta_w= allel.windowed_statistic(mut_positions, (mut_positions, samp_ac), allel.watterson_theta, size=int((genomeSize/nWin)*size_factor), start = 0, stop = int((genomeSize-1)*size_factor))
+            theta_w= allel.windowed_statistic(mut_positions, (mut_positions, samp_ac), allel.watterson_theta, windows = al_win)
             
             #theta_w = allel.windowed_watterson_theta(mut_positions, samp_ac,size = int(genomeSize/nWin)*1e6, start = 0) 
         
         ## calculate diversity
                 ## using tskit
-            div = samp_ts.diversity(sample_sets = None, windows = win)
+            div = samp_ts.diversity(sample_sets = None, windows = win)  ##fix windows
+            
+            
+    ## checks
+            tests = [div, tajdb, theta_w[0],hap_stats[0]]
+            for test in tests:
+                if len(test)!= nWin:
+                    print("error in test ", test, ", number of values does not match number of windows")
                 ## Collate data
             for w in range(nWin):           ##run through widnows
                # w_t = time.time()
@@ -267,7 +274,7 @@ def ts_analysis(sim_run):
                 dict3.update({"H1": hap_stats[0][w,0]})              ## H1
                 dict3.update({"H12":hap_stats[0][w,1]})             ## H12
                 dict3.update({"H123": hap_stats[0][w,2]})            ## H123
-                dict3.update({"H2/H1": hap_stats[0][w,3]})           ## H2/H1
+                dict3.update({"H2H1": hap_stats[0][w,3]})           ## H2/H1
               
                 rows_list3.append(dict3)
                # print("per win = ", (time.time()- w_t))
