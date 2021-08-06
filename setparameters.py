29379ff94fc1ad42a9c91639b9aa71aa989f8468
olivia-johnson <64459311+olivia-johnson@users.noreply.github.com>
2021-08-06 14:51:19 +0930

    Add in burnin Ne parameter

--- setparameters.py
+++ setparameters.py
@@ -1,34 +1,35 @@
 import yaml
 import os
 
-group = 28
-sim_type = "seglift_substitute"
+group = 1
+sim_type = "seglift_linked_allele"
 ##"seglift_long"
-os.chdir("/Users/olivia/oliviaphd/{0}".format(sim_type))
+os.chdir("/Users/olivia/phd_data/{0}".format(sim_type))
 path  = "./group_" + str(group)
 os.mkdir(path)
 
-os.chdir("/Users/olivia/oliviaphd/{0}/group_{1}".format(sim_type, group))
+os.chdir("/Users/olivia/phd_data/{0}/group_{1}".format(sim_type, group))
 
 parameters= {"sim_type": sim_type,
-"slim_sim": "seglift_long",
+"slim_sim": "seglift_linked_allele",
     ##"seglift_long",
 "group":group, 
 "runs" : 8,
-"nChrom":10,
-"chromSize":5e5,
-"recRate":1e-8,
+"nChrom":50,
+"chromSize":2,
+"recRate":0,
 "mutRate":3e-9,
 "s_pop":1e4,
-"w_pop":1e3,
-"l":10,
+"w_pop":1e4,
+"l":100,
 "y":0.5,
 "d":0.65,
-"rGen":375,
+"rGen":3000,
 "fitness_on":1,
-"sum_gen":13,
-"win_gen":2,
-"winpChrom":51}
+"sum_gen":10,
+"win_gen":10,
+"winpChrom":51,
+"burnin_Ne":0}
 
 with open('parameters.yml', 'w') as outfile:
     yaml.dump(parameters, outfile, default_flow_style=False)
