e03c6ff600c4c5010dc162e5d1f9309393ee3185
olivia-johnson <64459311+olivia-johnson@users.noreply.github.com>
2022-03-24 10:39:15 +1030

    refine parameters

--- hpc/hpc_parameters.py
+++ hpc/hpc_parameters.py
@@ -4,27 +4,56 @@ import yaml
 os.chdir("/Users/olivia/phd_data/hpc_parameters/")
 
 ##param_label = "CP_UG_L1000_d65_y05_allele"
-group =84
+group =29
+sim="background"
 
 parameters= {"group":group,
-"slim_sim": "hpc_seglift_l10",
+"slim_sim": sim,
 "nChrom":21,
 "chromSize":5e5,
-"recRate":1e-8,
-"mutRate":3e-9,
+"recRate":1e-6,
+"mutRate":1e-7,
 "s_pop":1e4,
 "w_pop":1e4,
-"l":1000,
+"l":10,
 "y":4,
 "d":0.65,
-"rGen":3000,
+"rGen":7500,
 "fitness_on":1,
-"sum_gen":15,
-"win_gen":15,
-"winpChrom":0,
+"sum_gen":13,
+"win_gen":2,
+"winpChrom":51,
 "burnin_Ne":1e4}
 
 
 
-with open('group_{0}.txt'.format(group), 'w') as outfile:
+with open('{1}/group_{0}.txt'.format(group,sim), 'w') as outfile:
+    yaml.dump(parameters, outfile, default_flow_style=False)
+
+
+group =4
+sim="capped"
+
+parameters= {"group":group,
+"slim_sim": sim,
+"nChrom":21,
+"chromSize":5e5,
+"recRate":1e-6,
+"mutRate":1e-7,
+"s_pop":1e4,
+"w_pop":1e4,
+"l":10,
+"y":4,
+"d":0.65,
+"rGen":7500,
+"fitness_on":1,
+"sum_gen":15,
+"win_gen":15,
+"winpChrom":51,
+"burnin_Ne":1e4,
+"offCap":350}
+
+
+
+with open('{1}/group_{0}.txt'.format(group,sim), 'w') as outfile:
     yaml.dump(parameters, outfile, default_flow_style=False)
