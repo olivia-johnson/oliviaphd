7f42db6bb61549a68bdd2afe12c326341f2c0ad1
olivia-johnson <64459311+olivia-johnson@users.noreply.github.com>
2023-02-22 17:19:37 +1030

    Test infiite sites to replace discrete genome mutation methods

--- r2_test/test_script.py
+++ r2_test/test_script.py
@@ -85,20 +85,25 @@ evens = h[:,1::2]
  
 gn=odds+evens
 gn=gn.view('int8')  ##input for r2 a gentoype array made up of 0,1,2
+
+np.argwhere(gn>2) ## values greater than 2
+
          
 print("Length of genotype data:",len(gn))
 print("Length of position vector:",len(mut_positions))
 print("Length of unique in position vector:",len(np.unique(mut_positions)))
 
 
-r2=allel.windowed_r_squared(mut_positions, gn, windows=al_win3)
+r2=allel.windowed_r_squared(mut_positions, gn, windows=al_win3, fill=None)
 print(r2[0])
 
 nan_start=np.take(r2[1][:, 0],np.where(np.isnan(r2[0]))) ## start of nan windows
 nan_end=np.take(r2[1][:, 1],np.where(np.isnan(r2[0]))) ##end of nan windows
-nans =np.reshape( np.concatenate((nan_start, nan_end), axis=0), (len(nan_start[0]), 2))  ## start and end of vector
+nans ={'start': nan_start, 'end': nan_end} ## start and end of vector
 
 print("No. sites with nan val:", sum(np.isnan(r2[0])))
+print("Max seg sites in segment:", max(r2[2]))
+print("Mainseg sites in segment:", min(r2[2]))
 print("Seg sites in window with nan val: ", np.take((r2[2]),np.where(np.isnan(r2[0]))) ) ##the seg sites of windows with nan values
 print("Windows of nan val:", nans)  ##the start position of windows with nan values
 
@@ -147,3 +152,63 @@ print("No. sites with nan val (ia):", sum(np.isnan(r2[0])))
 print("Seg sites in window with nan val (ia): ", np.take((r2[2]),np.where(np.isnan(r2[0]))) ) ##the seg sites of windows with nan values
 print("Windows of nan val (ia):", nans)  ##the start position of windows with nan values
 
+
+
+
+## testing infinitie sites  -NOT WORKING
+
+##*mut_pos by 1e10 makes unique muattion positions, messes up statistics
+mut_ts =msprime.sim_mutations(slim_ts, rate=mutRate, discrete_genome=False, keep=False)
+
+print("Number of mutations (ia):", mut_ts.num_mutations) ## number of mutations 
+print("Number of sites with mutations (ia):",mut_ts.num_sites)     ## number of sites that have mutations
+all_mutations = mut_ts.mutations_site  ## sites of all mutations (includes duplicate positions)
+duplicate_mutations = dupes(all_mutations)  ##identify duplicate positions
+dupe_no=mut_ts.num_mutations-mut_ts.num_sites  #numebr of duplicates
+print("No. duplicate mutations (ia):", str(dupe_no))
+    
+al_win3 = []
+for w in range(len(win3)-1):
+      if w == len(win3)-1:
+          window = [win3[w]*1e10, int(mut_ts.sequence_length)*1e10]
+      else:
+          window = [win3[w]*1e10, win3[w+1]*1e10-1]
+      al_win3.append(window)
+
+samp_gm=mut_ts.genotype_matrix()
+     # convert genotype matrix to haplotyoe array for haplotype statistics
+h= allel.HaplotypeArray(samp_gm)
+     # allele count for scikit.allel stats
+samp_ac = h.count_alleles()
+     # positions of mutations in samp_ts for scikit.allel windowed_statistic function
+mut_positions = [int(var.position*1e10+1) for var in mut_ts.variants()]  ## 
+dupes(mut_positions) ## positions that are dupicated
+ 
+     ## crete genotype array for LD
+odds = h[:,::2]
+evens = h[:,1::2]
+ 
+gn=odds+evens
+gn=gn.view('int8')  ##input for r2 a gentoype array made up of 0,1,2
+         
+print("Length of genotype data (ia):",len(gn))
+print("Length of position vector (ia):",len(mut_positions))
+print("Length of unique in position vector (ia):",len(np.unique(mut_positions)))
+
+
+r2=allel.windowed_r_squared(mut_positions, gn, windows=al_win3)
+print(r2[0])
+
+tajdb =  mut_ts.Tajimas_D(sample_sets=None, windows=win3, mode="branch")
+tajds =  mut_ts.Tajimas_D(sample_sets=None, windows=win3, mode="site")
+tajda= allel.windowed_tajima_d(mut_positions, samp_ac, windows=al_win3)
+tajds-tajda[0] ##differnce between tajimas d site from tskit and scikit allele tajimas d
+
+
+nan_start=np.take(r2[1][:, 0],np.where(np.isnan(r2[0]))) ## start of nan windows
+nan_end=np.take(r2[1][:, 1],np.where(np.isnan(r2[0]))) ##end of nan windows
+nans =np.reshape( np.concatenate((nan_start, nan_end), axis=0), (len(nan_start[0]), 2))  ## start and end of vector
+
+print("No. sites with nan val (ia):", sum(np.isnan(r2[0])))
+print("Seg sites in window with nan val (ia): ", np.take((r2[2]),np.where(np.isnan(r2[0]))) ) ##the seg sites of windows with nan values
+print("Windows of nan val (ia):", nans)  ##the start position of windows with nan values
