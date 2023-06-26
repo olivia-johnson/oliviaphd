4737ccccc1326f8a8685ff8b139b8fe6d92a4263
olivia-johnson <64459311+olivia-johnson@users.noreply.github.com>
2023-06-26 15:37:57 +0930

    Add condition to only calculate for invasive and equilibrium timepoints

--- hpc/300bp_single_locus_analysis.py
+++ hpc/300bp_single_locus_analysis.py
@@ -114,6 +114,14 @@ for w in range(len(win3)-1):
       al_win3.append(window)
 
     # cycle throught timepoints for which data has been collected
+end_time=max(ind_times)
+if sim_run <=20:
+    early_t=ind_times[ind_times>(end_time-5000)]
+    late_t= ind_times[ind_times<=(end_time-96000)]
+    late_t=late_t[late_t>((end_time-97000))]
+    ind_times=np.append(early_t, late_t)
+    
+    
 for t in ind_times:
 
         # collate nodes (geotype identifiers) of indviduals at time t
