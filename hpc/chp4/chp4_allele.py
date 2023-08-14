3c875168832a406ce64c92e4b11432d532dbde34
olivia-johnson <64459311+olivia-johnson@users.noreply.github.com>
2023-08-14 15:41:22 +0930

    Scripts for chapter 4 allele trajectory simulations

---
+++ hpc/chp4/chp4_allele.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import msprime
+import pyslim
+import numpy as np
+import yaml
+import tskit
+import pandas as pd
+import time
+import allel
+sys.path.insert(1, '/home/a1704225/oliviaphd/hpc/')
+import chp4_functions
+
+params=sys.argv[1]
+sim_run = sys.argv[2]
+tmpdir =str(sys.argv[3])
+####  READ IN PARAMETERS
+    # load in parameter file
+with open('/hpcfs/users/a1704225/parameters/chp4_alleles/{0}.txt'.format(params), 'r') as f:
+    parameters = yaml.load(f, Loader=yaml.FullLoader)
+
+    #set parameters from file
+s_pop = int(parameters["s_pop"])
+w_pop = int(parameters["w_pop"])
+l = int(parameters["l"])
+y = parameters["y"]
+rGen=int(parameters["rGen"])
+fitness_on = parameters["fitness_on"]
+sum_gen = int(parameters["sum_gen"])
+win_gen = int(parameters["win_gen"])
+group=parameters["group"]
+
+start_time = time.time()
+####  SIMULATE SEGLIFT
+chp4_functions.simulate_alleles(tmpdir, group, sim_run, s_pop, w_pop, l, y, rGen, fitness_on, sum_gen, win_gen)
+
+print("Time = ", (time.time() - start_time))
---
+++ hpc/chp4_functions.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import msprime
+import pyslim
+import numpy as np
+import tskit
+import pandas as pd
+import time
+import itertools
+import allel
+import statistics
+import scipy
+sys.path.insert(1, '/hpcfs/users/a1704225/oliviaphd/hpc/')
+import NCD
+
+
+def simulate_alleles(tmpdir, group, sim_run, s_pop, w_pop, l, y, rGen, fitness_on, sum_gen, win_gen):
+        genomeSize = l
+
+        ## FORWARD SIMULATION
+        # for when uneven seasons " -d g_s=" + str(sum_gen)+ " -d g_w=" + str(win_gen)
+        start_time = time.time()
+        tmpdir_call = "tmpdir='" + str(tmpdir)+ "'"
+
+        cmd = 'slim -d "' +str(tmpdir_call)+ '" -d fit='+ str(fitness_on)+" -d group=" + str(group) + " -d g_s=" + str(sum_gen)+" -d g_w=" + str(win_gen)+" -d sim_run=" + str(sim_run) + " -d GenomeSize=" + str(int(genomeSize)) + " -d L=" + str(l)+ " -d n_s=" + str(int(s_pop)) + " -d n_w=" + str(int(w_pop)) + " -d y=" + str(y) + " -d rGen="+ str(rGen) +" ~/oliviaphd/hpc/chp4/witt_complex_allele.slim"
+
+        print(cmd)
+        os.system(cmd)
+        # #print("Time for SLiM sim = ", (time.time()- start_time))
+        print("Simulations took ", (time.time()-start_time) ,  " seconds")
