b05a9c6597fb0f646ac0aefbe180a5809acd6035
olivia-johnson <64459311+olivia-johnson@users.noreply.github.com>
2023-10-27 17:48:04 +1030

    Update chp4_functions.py

--- hpc/chp4_functions.py
+++ hpc/chp4_functions.py
@@ -22,7 +22,7 @@ def simulate_alleles(tmpdir, group, sim_run, s_pop, w_pop, l, y, rGen, fitness_o
         start_time = time.time()
         tmpdir_call = "tmpdir='" + str(tmpdir)+ "'"
 
-        cmd = 'slim -d "' +str(tmpdir_call)+ '" -d fit='+ str(fitness_on)+" -d group=" + str(group) + " -d g_s=" + str(sum_gen)+" -d g_w=" + str(win_gen)+" -d sim_run=" + str(sim_run) + " -d GenomeSize=" + str(int(genomeSize)) + " -d L=" + str(l)+ " -d n_s=" + str(int(s_pop)) + " -d n_w=" + str(int(w_pop)) + " -d y=" + str(y) + " -d rGen="+ str(rGen) +" ~/oliviaphd/hpc/chp4/witt_complex_allele.slim"
+        cmd = 'slim -d "' +str(tmpdir_call)+ '" -d fit='+ str(fitness_on)+" -d group=" + str(group) + " -d g_s=" + str(sum_gen)+" -d g_w=" + str(win_gen)+" -d sim_run=" + str(sim_run) + " -d GenomeSize=" + str(int(genomeSize)) + " -d L=" + str(l)+ " -d n_s=" + str(int(s_pop)) + " -d n_w=" + str(int(w_pop)) + " -d y=" + str(y) + " -d rGen="+ str(rGen) +" /localscratch/oliviaphd/hpc/chp4/witt_complex_allele.slim"
 
         print(cmd)
         os.system(cmd)
